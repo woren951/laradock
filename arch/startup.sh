63dc92322985573ce4f57411004a644483e63953
o.kotyay <o.kotyay@ideil.com>
2022-03-08 23:49:26 +0200

    arch update

--- arch/startup.sh
+++ arch/startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Start php-fpm in foreground
-$HOME/.phpenv/versions/7.4.27/etc/init.d/php-fpm start
-$HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
-$HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
+# $HOME/.phpenv/versions/7.4.27/etc/init.d/php-fpm start
+# $HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
+# $HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
 
 # Start nginx in foreground
 nginx
