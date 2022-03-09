5e9c76441157528b3dc4cf74f232e14016200db5
o.kotyay <o.kotyay@ideil.com>
2022-03-09 23:34:11 +0200

    asdf php

--- arch/startup.sh
+++ arch/startup.sh
@@ -5,5 +5,10 @@
 # $HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
 # $HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
 
+# Start php-fpm in foreground
+$HOME/.asdf/installs/php/7.4.28/bin/php-fpm
+$HOME/.asdf/installs/php/8.0.16/bin/php-fpm
+$HOME/.asdf/installs/php/8.1.3/bin/php-fpm
+
 # Start nginx in foreground
 nginx
