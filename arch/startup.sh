3b167f2214b4d7555d6cdde42ec12596ad9c9325
o.kotyay <o.kotyay@ideil.com>
2022-03-10 18:19:48 +0200

    locales added

--- arch/startup.sh
+++ arch/startup.sh
@@ -4,11 +4,9 @@
 # $HOME/.phpenv/versions/7.4.27/etc/init.d/php-fpm start
 # $HOME/.phpenv/versions/8.0.16/etc/init.d/php-fpm start
 # $HOME/.phpenv/versions/8.1.3/etc/init.d/php-fpm start
-
-# Start php-fpm in foreground
-$HOME/.asdf/installs/php/7.4.28/bin/php-fpm
-$HOME/.asdf/installs/php/8.0.16/bin/php-fpm
-$HOME/.asdf/installs/php/8.1.3/bin/php-fpm
+$HOME/.asdf/installs/php/7.4.28/sbin/php-fpm
+$HOME/.asdf/installs/php/8.0.16/sbin/php-fpm
+$HOME/.asdf/installs/php/8.1.3/sbin/php-fpm
 
 # Start nginx in foreground
 nginx
